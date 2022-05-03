686a58c30f93e260947234d953b06e1b7134b763
BASSEM <bassem.jadoui@esprit.tn>
2022-05-03 14:27:22 +0100

    Initial commit

---
+++ src/main/java/com/spring/gestionrevenue/GestionRevenueApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gestionrevenue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRevenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionRevenueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/gestionrevenue/GestionRevenueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gestionrevenue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionRevenueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
